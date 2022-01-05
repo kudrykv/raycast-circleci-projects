22736455ec46b1e89bb7b53bda0cd5eedcd465f5
Vitaliy Kudryk <kudryk.v@protonmail.com>
2022-01-05 17:04:04 +0200

    env vars

--- src/ListCircleCIProjects.tsx
+++ src/ListCircleCIProjects.tsx
@@ -21,27 +21,31 @@ export const ListCircleCIProjects = ({ isLoading, uris, onReload, onListAllEnvs 
   </List>;
 
 
-const CircleCIItem = ({
-                        uri,
-                        name,
-                        onReload,
-                        onListAllEnvs
-                      }: { uri: string, name: string, onReload: () => void, onListAllEnvs: (uri: string) => Promise<Record<string, string>[]> }) =>
+interface CircleCIItemParams {
+  uri: string;
+  name: string;
+  onReload: () => void;
+  onListAllEnvs: (uri: string) => Promise<Record<string, string>[]>;
+}
+
+const CircleCIItem = ({ uri, name, onReload, onListAllEnvs }: CircleCIItemParams) =>
   <List.Item
     title={name}
     icon={{ source: { light: "icon.png", dark: "icon@dark.jpg" } }}
-    actions={
-      <ActionPanel>
-        <OpenInBrowserAction url={`https://app.circleci.com/pipelines/github/${name}`} />
-        <SubmitFormAction onSubmit={onReload} title="Refresh projects list" icon={Icon.ArrowClockwise} />
-        <PushAction
-          title={"List environment variables"}
-          shortcut={{ key: "e", modifiers: ["cmd", "shift"] }}
-          target={<ListCircleCIEnvVariables uri={uri} full_name={name} onListAllEnvs={onListAllEnvs} />}
-        />
-      </ActionPanel>
-    }
+    actions={<CircleCIItemActions uri={uri} name={name} onReload={onReload} onListAllEnvs={onListAllEnvs} />}
   />;
+
+
+const CircleCIItemActions = ({uri, name, onReload, onListAllEnvs}: CircleCIItemParams) =>
+  <ActionPanel>
+    <OpenInBrowserAction url={`https://app.circleci.com/pipelines/github/${name}`} />
+    <SubmitFormAction onSubmit={onReload} title="Refresh projects list" icon={Icon.ArrowClockwise} />
+    <PushAction
+      title={"List environment variables"}
+      shortcut={{ key: "e", modifiers: ["cmd", "shift"] }}
+      target={<ListCircleCIEnvVariables uri={uri} full_name={name} onListAllEnvs={onListAllEnvs} />}
+    />
+  </ActionPanel>
 
 
 const mapURI = (onReload: () => void, onListAllEnvs: (uri: string) => Promise<Record<string, string>[]>) =>
